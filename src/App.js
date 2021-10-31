0b339f27fed82a96ca5396ba854ccc632f4fb5a1
Diego Quintela <diegolucasq@gmail.com>
2021-10-30 22:23:28 -0300

    add a new Expense component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import ExpenseItem from "./components/ExpenseItem";
+import Expense from "./components/Expense";
 
 function App() {
   return (
     <div className="App">
       <h1>First steps</h1>
-      <ExpenseItem />
+      <Expense />
     </div>
   );
 }
